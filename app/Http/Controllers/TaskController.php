23d015a099c553154c433b3f3e1e654beec75e2e
beatriz <beatriz.zuri@rocketfy.es>
2021-09-27 19:36:39 +0200

    adding views and libraries

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,14 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 use DB;
+use App\Models\Task;
+use App\Models\Category;
 
 class TaskController extends Controller
 {
 
     public function index()
     {
-        return view('tasks.browse');
-    }
+
+        $tasks = Task::all();
+        $categories = Category::all();
+
+        return view('tasks.browse', compact([
+            'tasks',
+            'categories'
+        ]));
+    } 
 
     
 }
--- resources/views/tasks/browse.blade.php
+++ resources/views/tasks/browse.blade.php
@@ -1,27 +1,69 @@
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.css">
+
 <div class="card-content">
 	<div class="card-body">
        	<div class="card-text">
-			<table class="table table-striped" style="width: 100%">
+
+		   <table class="table  table-borderless" style="width: 80%; margin-left: 10%;">
+				<tbody>
+					<tr>
+						<th scope="col" style="width: 30%">
+							Nombre
+						</th>
+						<th scope="col" style="width: 60%">
+							Categorías
+						</th>
+						<th scope="col" style="width: 10%">
+							Añadir
+						</th>
+					</tr>
+					<tr>
+						<td><input type='text' class='form-control' id='name' style="height: 28px !important;"/></td>
+						<td>
+							<select id="categories" class=" form-control select2">
+								@foreach($categories as $category)
+									<option value='{{ $category->id }}'>{{$category->name}}</option>
+								@endforeach
+							</select>
+						</td>
+						<td>
+							<button type="button" class="btn btn-success" style="font-size:11px;">Añadir</button>
+						</td>
+					</tr>
+				</tbody>
+			</table> 
+
+		   <table class="table table-striped" style="width: 80%; margin-left: 10%;">
 				<thead>
 					<tr>
-                        <th>Tarea</th>
-                        <th>Categoría</th>
-                        <th>Acciones</th>
+					<th scope="col" style="width: 30%">Nombre</th>
+					<th scope="col" style="width: 60%">Categoría/s</th>
+					<th scope="col" style="width: 10%">Eliminar</th>
 					</tr>
 				</thead>
-				<tbody>
-                    @foreach ($tasks as $task)
-                        
-                    @endforeach
+				<tbody id="tbody_tasks">
+					@foreach ($tasks as $task)
+						<tr>
+						<td>{{$task->name}}</td>
+						<td>{{$task->categories}}</td>
+						<td><button type="button" class="btn btn-danger" style="font-size:11px;">Eliminar</button></td>
+						</tr>
+					@endforeach
 				</tbody>
-				<tfoot>
-					<tr class="tfoot-row">
-						<td></td>
-                        <td></td>
-                        <td></td>
-					</tr>
-				</tfoot>
 			</table>
 		</div>
 	</div>
 </div>
+
+
+<script src="{{asset('js/jquery-min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.full.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/i18n/es.js"></script>
+
+<script>
+$('.select2').select2({
+			language: 'es',
+			width: '100%'
+});
+</script>
