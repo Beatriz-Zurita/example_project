f1d725802f6611be70ce49dd66c20b25e5cece99
beatriz <beatriz.zuri@rocketfy.es>
2021-09-27 22:15:14 +0200

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('categories')->insert([
+            'name'         => 'Java'
+        ]);
+
+        DB::table('categories')->insert([
+            'name'         => 'PHP'
+        ]);
+
+        DB::table('categories')->insert([
+            'name'         => 'HTML'
+        ]);
     }
 }
